b560699c3c8359f68f382bff9ab39b146013f5ca
zinodev0 <zinodev0@gmail.com>
2022-07-09 09:26:21 +0100

    Products and products detail

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/"> ESound Store</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick=""></button>
+
+      <AiOutlineShopping style={{ width: "25px", height: "25px" }} />
+      <span className="cart-item-qty">1</span>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,11 @@ const index = ({ products, bannerData }) => {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
