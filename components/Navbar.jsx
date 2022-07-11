0215c220085c00b9059969fa464c7d46302f8c23
zinodev0 <zinodev0@gmail.com>
2022-07-11 19:36:16 +0100

    Added Context

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+  const { qty } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/"> ESound Store</Link>
       </p>
-      <button type="button" className="cart-icon" onClick=""></button>
-
-      <AiOutlineShopping style={{ width: "25px", height: "25px" }} />
-      <span className="cart-item-qty">1</span>
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{qty}</span>
+      </button>
+      <Cart />
     </div>
   );
 };
